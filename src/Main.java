07b8adce372757c2d3f5d73bf9f3c85760846491
Tina Gosarič <tina.gosaric@gorenje.com>
2022-10-25 13:53:08 +0200

    Everything about generics.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FootballPlayer joel = new FootballPlayer("Joel");
+        FootballPlayer mik = new FootballPlayer("Mik");
+        FootballPlayer liam = new FootballPlayer("Liam");
+        BaseballPlayer cat = new BaseballPlayer("Cat");
+        SoccerPlayer xavi = new SoccerPlayer("Xavi");
+
+        Team<FootballPlayer> footballTeam1 = new Team<>("Adelaide Crows");
+        footballTeam1.addPlayer(joel);
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(cat);
+
+        Team<SoccerPlayer> soccerTeam = new Team<>("Barcelona");
+        soccerTeam.addPlayer(xavi);
+
+        Team<FootballPlayer> footballTeam2 = new Team<>("Melbourne");
+        footballTeam2.addPlayer(mik);
+
+        Team<FootballPlayer> footballTeam3 = new Team<>("Hawthorn");
+        footballTeam3.addPlayer(liam);
+
+        footballTeam1.matchResult(footballTeam2, 1,0);
+        footballTeam1.matchResult(footballTeam3, 3, 8);
+
+        System.out.println("Rankings");
+        System.out.println(footballTeam1.getName() + ": " + footballTeam1.ranking());
+        System.out.println(footballTeam2.getName() + ": " + footballTeam2.ranking());
+        System.out.println(footballTeam3.getName() + ": " + footballTeam3.ranking());
+
+        System.out.println(footballTeam1.compareTo(footballTeam2));
+        System.out.println(footballTeam2.compareTo(footballTeam3));
     }
 }
